39de70f91f2db02ce89da17750851ea5ab03591d
mikol-ux <okoyeikechukwu60@gmail.com>
2025-03-21 16:49:56 +0100

    fourth,,,, commit

--- app/sections/Myservices/index.tsx
+++ app/sections/Myservices/index.tsx
@@ -31,8 +31,8 @@ export default function Services() {
   return (
     <section className="contained bg-black">
       <div className="text-center mb-12">
-        <h2 className="text-4xl font-semibold">My Services</h2>
-        <p className="text-2xl text-gray-600 mt-2 max-w-2xl mx-auto">
+        <h2 className="text-4xl font-semibold text-white">My Services</h2>
+        <p className="text-2xl text-gray-400 mt-2 max-w-2xl mx-auto">
           I specialize in creating intuitive designs, modern websites, and
           engaging digital experiences.
         </p>
